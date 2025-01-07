e9adea19c4b94989bc2641cdec06e83f48985719
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2025-01-07 11:12:51 +0530

    second batman

--- fs.cpp
+++ fs.cpp
@@ -6,14 +6,12 @@
 
 #include "fs.hpp"
 
-std::array<char, 301> inode::inode_to_bytestream() {
-    std::array<char, 301> buffer;
+auto inode::inode_to_bytestream() {
+    std::array<char, INODE_BYTESTREAM_LENGTH> buffer;
     std::size_t offset = 0;
 
-    for (const auto & child : children) {
-        std::memcpy(buffer.data() + offset, &(child->inode_number), sizeof(child->inode_number));
-        offset += sizeof(child->inode_number);
-    }
+    std::memcpy(buffer.data(), children.data(), children.size());
+    offset = children.size();
 
     std::memcpy(buffer.data() + offset, name.data(), 32);
     offset += 32;
@@ -32,18 +30,36 @@ std::array<char, 301> inode::inode_to_bytestream() {
     return buffer;
 }
 
-fs::fs(const std::string& file_name) : file_name(file_name) {
+fs::fs(const std::string& file_name) : file_name(file_name), fs_handler(file_name, std::ios::binary) {
     root = std::make_shared<inode>();
     root->is_file = false;
     root->size = 0;
-    root->inode_number = 0;
+    root->inode_number = latest_inode = 0;
     inode_map.insert({"/", root});
 }
 
-bool fs::init_fs() {
-    std::ofstream fs_file(file_name, std::ios::binary);
+auto fs::init_fs() {
 
 
 }
 
+auto fs::persist_inode(std::shared_ptr<inode> c_inode) {
+    auto inode_bytestream = c_inode->inode_to_bytestream();
+    fs_handler.seekp(INODE_OFFSET_LENGTH + c_inode->inode_number * INODE_BYTESTREAM_LENGTH);
+    fs_handler << inode_bytestream.data();
+    return fs_handler.fail();
+}
+
+auto fs::fetch_inode(const unsigned int inode_number) {
+    fs_handler.seekp(INODE_OFFSET_LENGTH + inode_number * INODE_BYTESTREAM_LENGTH);
+
+    auto in = std::make_shared<inode>();
+    fs_handler.read(in->children.data(), in->children.size());
+
+    
+
+
+
+
+}
 
--- fs.hpp
+++ fs.hpp
@@ -11,8 +11,11 @@ struct inode {
     unsigned int inode_number;          // 4 bytes
     unsigned int starting_block_number;          // 4 bytes
                                         // Total = 301 bytes
-    std::array<char, 301> inode_to_bytestream();
+    auto inode_to_bytestream();
 };
+
+const unsigned int INODE_OFFSET_LENGTH = 64;
+const unsigned int INODE_BYTESTREAM_LENGTH = 301;
 
 template <unsigned int BLOCK_SIZE = 1024>
 struct block {
@@ -26,12 +29,18 @@ using block2k = block<2048>;
 class fs {
     
     std::string file_name;
+    std::fstream fs_handler;
     std::shared_ptr<inode> root;
     std::unordered_map<std::string, std::shared_ptr<inode>> inode_map;
+    unsigned int latest_inode;
 
 public:
     fs(const std::string& file_name);
 
-    bool init_fs();
+    auto persist_inode(std::shared_ptr<inode> inode);
+
+    auto fetch_inode(const unsigned int inode_number);
+
+    auto init_fs();
 
 };
