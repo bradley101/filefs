b28ee1074fd728bb148a2f2ccae3fcdcec88f4ca
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2025-05-14 20:12:38 +0530

    some more progress

--- src/block.rs
+++ src/block.rs
@@ -3,15 +3,24 @@
 const BLOCK_SIZE: usize = 4 << 10;
 const BLOCK_DATA_SIZE: usize = BLOCK_SIZE - 8;
 
-use bitmaps::Bitmap;
-
+use bitvec::prelude::*;
 
 pub struct SuperBlock {
     version: u16,
     total_inodes: u16,
     total_blocks: u16,
+    free_inodes: u16,
+    free_blocks: u16,
     inode_size_log: u8,
     block_size_log: u8,
+}
+
+pub struct InodeBitmap {
+    bitmap: BitVec<u8>
+}
+
+pub struct BlockBitmap {
+    bitmap: BitVec<u8>
 }
 
 pub struct Block {
