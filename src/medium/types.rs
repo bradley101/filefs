fd7f8839960f3a177cf954219cc42a825f251c23
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2025-10-31 17:23:56 +0530

    added byte read write compatible types for data. currently only implemented for files

--- src/medium/types.rs
+++ src/medium/types.rs
@@ -6,6 +6,6 @@ pub enum medium {
 }
 
 pub trait byte_compatible {
-    fn read_all() -> Result<Vec<u8>, Error>;
-    fn write_all() -> Result<(), Error>;
+    fn read_all(&self, offset: u64, len: usize, buffer: &mut [u8]) -> Result<(), Error>;
+    fn write_all(&self, offset: u64, len: usize, bufffer: &[u8]) -> Result<(), Error>;
 }
